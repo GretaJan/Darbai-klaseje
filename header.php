3fed6d22144aa6189fd74f05308156b9eb5eafcf
DESKTOP-SNLPAFP\greta <gretajan09@gmail.com>
2018-10-09 16:17:12 +0300

    testas

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="css/style.css">
+   <title>pirmas uždavinys</title>
+</head>
+<body>
+<div class="proc80">
+    <p>Lorem ipsum dolor sit amet, in est adhuc regione. Ne quando consequat adipiscing eum. Cum solet mucius te, paulo everti molestiae at vim. Alia animal sensibus eu has. Hinc aliquam pertinax sed ei, est munere scripta accusam an.</p>
+</div>
+<div class="proc20">
+    <p>Id mentitum democritum sed, eu magna illud admodum nam.Id mentitum democritum sed, eu magna illud admodum nam. Animal saperet dolorem id vim, recteque scriptorem repudiandae eum at. Nam case vocibus intellegam ex, vix</p>
+</div>
+</body>
